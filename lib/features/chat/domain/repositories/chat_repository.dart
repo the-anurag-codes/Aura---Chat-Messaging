dece5af5aa49add5229b6f0bd924709edfaa4b1e
the-anurag-codes <auragchauhan48177@gmail.com>
2025-11-02 15:06:01 +0530

    feat: implement chat repository interface

--- lib/features/chat/domain/repositories/chat_repository.dart
+++ lib/features/chat/domain/repositories/chat_repository.dart
@@ -3,19 +3,28 @@ import '../entities/message_entity.dart';
 import '../../../../core/errors/failure.dart';
 
 abstract class ChatRepository {
-  Future<Either<Failure, void>> connect(String userId);
-
-  Future<Either<Failure, void>> disconnect();
-
+  /// Send a message to Firestore
   Future<Either<Failure, void>> sendMessage(MessageEntity message);
 
-  Stream<MessageEntity> get messageStream;
+  /// Get messages stream between two users
+  Stream<Either<Failure, List<MessageEntity>>> getMessagesStream({
+    required String userId,
+    required String otherUserId,
+  });
 
-  Stream<bool> get typingStream;
-
+  /// Send typing indicator
   Future<Either<Failure, void>> sendTypingIndicator({
-    required String senderId,
-    required String receiverId,
+    required String userId,
+    required String otherUserId,
     required bool isTyping,
   });
+
+  /// Get typing indicator stream
+  Stream<bool> getTypingStream({
+    required String userId,
+    required String otherUserId,
+  });
+
+  /// Mark message as read
+  Future<Either<Failure, void>> markMessageAsRead({required String messageId});
 }
